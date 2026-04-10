86b598588385eb604006e49f28cef1de5f9e42aa
Majestic <chaukechinedu@gmail.com>
2026-04-10 08:58:07 +0200

    added variables to multiplicationTable.kt

--- MultiplicationApk/app/src/main/java/com/example/multiplicationapk/MultiplicationTable.kt
+++ MultiplicationApk/app/src/main/java/com/example/multiplicationapk/MultiplicationTable.kt
@@ -1,6 +1,7 @@
 package com.example.multiplicationapk
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,12 @@ class MultiplicationTable : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_multiplication_table)
+
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("tablenumber")
+        val tableNumber = tableString?.toInt()
+        val multiplicationTable = findViewById<TextView>(R.id.multiplicationTableTextView)
+        multiplicationTable.text = "$tableNumber x table"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
